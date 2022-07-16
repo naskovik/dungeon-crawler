88afe1737e4cbe84e32718505ac11ad601f21825
naskovik <nazarskoreyko@gmail.com>
2022-07-16 13:08:41 +0200

    Fixes

--- src/spawner.rs
+++ src/spawner.rs
@@ -1,14 +1,38 @@
 use crate::prelude::*;
 
-pub fn spawn_player(ecs: &mut World, pos: Point) {
+pub fn spawn_player(ecs : &mut World, pos : Point) {
     ecs.push(
-        (   Player,
+        (Player,
             pos,
-            Render {
+            Render{
                 color: ColorPair::new(WHITE, BLACK),
-                glyph: to_cp437('@')
+                glyph : to_cp437('@')
             },
-            Health { current: 20, max: 20 }
+            Health{ current: 20, max: 20 }
+        )
+    );
+}
+
+pub fn spawn_monster(
+    ecs: &mut World,
+    rng: &mut RandomNumberGenerator,
+    pos : Point
+) {
+    let (hp, name, glyph) = match rng.roll_dice(1,10) {
+        1..=8 => goblin(),
+        _ => orc()
+    };
+
+    ecs.push(
+        (Enemy,
+            pos,
+            Render{
+                color: ColorPair::new(WHITE, BLACK),
+                glyph,
+            },
+            MovingRandomly{},
+            Health{current: hp, max: hp},
+            Name(name)
         )
     );
 }
@@ -19,30 +43,4 @@ fn goblin() -> (i32, String, FontCharType) {
 
 fn orc() -> (i32, String, FontCharType) {
     (2, "Orc".to_string(), to_cp437('o'))
-}
-
-pub fn spawn_monster(
-    ecs: &mut World,
-    rng: &mut RandomNumberGenerator,
-    pos: Point
-) {
-
-    let (hp, name, glyph) =
-        match rng.roll_dice(1,10) {
-            1..=8 => goblin(),
-            _ => orc(),
-        };
-
-    ecs.push(
-        (   Enemy,
-            pos,
-            Render {
-                color: ColorPair::new(WHITE, BLACK),
-                glyph,
-            },
-            MovingRandomly{},
-            Health { current: hp, max: hp },
-            Name(name)
-        )
-    );
 }
