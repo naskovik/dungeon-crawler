5abf04d07477285563b995847543de25d11ce90c
naskovik <nazarskoreyko@gmail.com>
2022-08-20 23:15:26 +0200

    Style fixes

--- src/map_builder/automata.rs
+++ src/map_builder/automata.rs
@@ -4,7 +4,9 @@ use super::MapArchitect;
 pub struct CellularAutomataArchitect {}
 
 impl MapArchitect for CellularAutomataArchitect {
+
     fn new(&mut self, rng: &mut RandomNumberGenerator) -> MapBuilder {
+
         let mut mb = MapBuilder {
             map: Map::new(),
             rooms: Vec::new(),
@@ -24,6 +26,7 @@ impl MapArchitect for CellularAutomataArchitect {
         mb.amulet_start = mb.find_most_distant();
 
        mb
+
     }
 
 }
@@ -35,6 +38,7 @@ impl CellularAutomataArchitect {
         map: &mut Map
     )
     {
+
         map.tiles.iter_mut().for_each(|t| {
             let roll = rng.range(0, 100);
             if roll > 55 {
@@ -44,18 +48,23 @@ impl CellularAutomataArchitect {
                 *t = TileType::Wall;
             }
         });
+
     }
 
     fn count_neighbours(&self, x: i32, y: i32, map: &Map) -> usize {
         let mut neighbours = 0;
         for iy in -1..= 1 {
+
             for ix in -1..=1 {
+
                 if !(ix == 0 && iy == 0)
                     && map.tiles[map_idx(x+ix, y+iy)] == TileType::Wall
                 {
                     neighbours += 1;
                 }
+
             }
+
         }
         neighbours
     }
@@ -63,7 +72,9 @@ impl CellularAutomataArchitect {
     fn iteration(&mut self, map: &mut Map) {
         let mut new_tiles = map.tiles.clone();
         for y in 1..SCREEN_HEIGHT -1 {
+
             for x in 1..SCREEN_WIDTH - 1 {
+
                 let neighbours = self.count_neighbours(x, y, map);
                 let idx = map_idx(x, y);
                 if neighbours > 4 || neighbours == 0 {
@@ -72,7 +83,9 @@ impl CellularAutomataArchitect {
                 else {
                     new_tiles[idx] = TileType::Floor;
                 }
+
             }
+
         }
         map.tiles = new_tiles;
     }
@@ -81,9 +94,15 @@ impl CellularAutomataArchitect {
         let center = Point::new(SCREEN_WIDTH /2, SCREEN_HEIGHT / 2);
         let closest_point = map.tiles.iter().enumerate()
             .filter(|(_, t)| **t == TileType::Floor)
-            .map(|(idx, _)| (idx, DistanceAlg::Pythagoras.distance2d(
-                center,
-                map.index_to_point2d(idx)))
+            .map(|(idx, _)|
+
+                {
+                    ( idx,
+                        DistanceAlg::Pythagoras.distance2d (
+                            center, map.index_to_point2d(idx))
+                    )
+                }
+
             )
             .min_by(|(_, distance), (_, distance2)|{
                 distance.partial_cmp(&distance2).unwrap()
