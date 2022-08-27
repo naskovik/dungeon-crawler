e558f96cde539450020e408fa3c421e038ab5a2b
naskovik <nazarskoreyko@gmail.com>
2022-08-27 12:00:42 +0200

    Render current level

--- src/systems/hud.rs
+++ src/systems/hud.rs
@@ -33,10 +33,23 @@ pub fn hud(ecs: &SubWorld) {
         ColorPair::new(WHITE, RED)
     );
 
-    let player = <(Entity, &Player)>::query()
+    let (player, map_level) = <(Entity, &Player)>::query()
         .iter(ecs)
-        .find_map(|(entity, _player)| Some(*entity))
+        .find_map(|(entity, player)| Some((*entity, player.map_level)))
         .unwrap();
+
+    draw_batch.print_color_right(
+        Point::new(SCREEN_WIDTH * 2, 1),
+        format!("Dungeon level: {}", map_level),
+        ColorPair::new(YELLOW, BLACK)
+    );
+
+    /*
+    let player = <(Entity, &Player)>::query()
+    .iter(ecs)
+    .find_map(|(entity, _player)| Some(*entity))
+    .unwrap();
+    */
 
     let mut item_query = <(&Item, &Name, &Carried)>::query();
     let mut y = 3;
